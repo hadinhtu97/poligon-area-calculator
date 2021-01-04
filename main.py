f0446c4438bb2b71db8eee6dd57f64eba3b62f03
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-01-04 13:13:11 +0700

    start

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from lorem import lorem
+import shape_calculator
+
+
+# rect = shape_calculator.Rectangle(5, 10)
+# print(rect.get_area())
+# rect.set_width(3)
+# print(rect.get_perimeter())
+# print(rect)
+
+# sq = shape_calculator.Square(9)
+# print(sq.get_area())
+# sq.set_side(4)
+# print(sq.get_diagonal())
+# print(sq)
